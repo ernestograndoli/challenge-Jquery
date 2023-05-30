86c992b28215fe75872d06909b7ffe1902e613cf
Grandoli, Ernesto <ernestograndoli@gmail.com>
2023-05-30 15:12:08 -0300

    Menu bottom Mobile Sticky

--- routes/views.router.js
+++ routes/views.router.js
@@ -1,14 +1,16 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const stockPrices = require('../data/stock-price')
+const stockPrices = require("../data/stock-price");
 
+router.get("/", (req, res) => {
+  res.render("products", {
+    title: "Product List",
+    styles: ["css/products.css"],
+  });
+});
 
-router.get('/', (req, res) => {
-    res.render('products', { title: 'Product List', styles: ["css/products.css"] })
-})
-
-router.get('*', (req, res) => {
-    res.render('product-detail', { title: 'Product Detail', styles: [] })
-})
+router.get("*", (req, res) => {
+  res.render("product-detail", { title: "Product Detail", styles: [] });
+});
 
 module.exports = router;
