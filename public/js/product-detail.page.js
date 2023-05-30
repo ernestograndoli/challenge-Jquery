d3fe9a1bbf89868eddc43581978057839f9fb7eb
Ferreira <pablo.ferreira@dxc.com>
2023-05-30 02:50:24 -0300

    Se agrega refresh de stock/price

--- public/js/product-detail.page.js
+++ public/js/product-detail.page.js
@@ -4,14 +4,24 @@ import { productUrlEncode } from './utils.js'
 
 const INTERVAL_REFRESH_MS = 5000
 
-const fetchPrices = async (code) => {
-    const response = await ProductsService.getStockPrice(code)
-    const { stock, price } = await response.json()
-
-    console.log(stock, price)
-
-    setTimeout(fetchPrices, INTERVAL_REFRESH_MS)
+const updateView = (product, priceStock) => {
+    console.log(document.getElementById("price"))
+    document.getElementById("brand").innerHTML = product.brand
+    document.getElementById("origin").innerHTML = product.origin
+    document.getElementById("description").innerHTML = product.information
+    document.getElementById("price").innerHTML = parseInt(priceStock.price) / 100
+    document.getElementById("stock").innerHTML = priceStock.stock
 }
+
+const fetchPriceStock = async (product) => {
+    const response = await ProductsService.getStockPrice(product.skus[0].code)
+    const stockPrice = await response.json()
+
+    updateView(product, stockPrice)
+
+    setTimeout(async () => await fetchPriceStock(product), INTERVAL_REFRESH_MS)
+}
+
 const currentProductIdBrand = window.location.pathname.substring(1)
 
 const product = ProductsService.getAll().find(i => productUrlEncode(i.id, i.brand) == currentProductIdBrand)
@@ -19,4 +29,4 @@ const product = ProductsService.getAll().find(i => productUrlEncode(i.id, i.bran
 if (!product)
     window.location.replace("/")
 
-fetchPrices(product)
+fetchPriceStock(product)
